864f1a325a3f470d0d1a63bbe0ce6ba710bc1f30
Git-Mon950 <subornac446@gmail.com>
2025-04-17 23:43:06 +0900

    Update Payments

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -27,5 +27,6 @@ class StripeCheckoutView(APIView):
             return redirect(checkout_session.url)
         except:
             return Response(
+                {'error': 'Something went wrong when creating stripe checkout session'},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
